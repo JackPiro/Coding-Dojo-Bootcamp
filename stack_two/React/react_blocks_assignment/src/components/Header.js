24debbec88f968ecbf9e199347fef1b7e551ab49
Jack Piro <jackpiro1133@gmail.com>
2023-03-28 22:37:59 -0700

    blocks assignment

---
+++ stack_two/React/react_blocks_assignment/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = (props) => {
+    return(
+        <div style = {{ backgroundColor: "#6aa84f", margin: "10px"}}>
+
+        </div>
+    );
+}
+
+export default Header;
